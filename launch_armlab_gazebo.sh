f09d74f8a1b26b856f7d8a47952dfebc2e4de467
Peter Gaskell <pgaskell@umich.edu>
2020-11-20 15:39:04 -0500

    apriltags working in sim

--- launch_armlab_gazebo.sh
+++ launch_armlab_gazebo.sh
@@ -5,4 +5,6 @@ gnome-terminal -- python ./gazebo_shim.py
 sleep 5
 gnome-terminal -- ./gazebo_shim_rs/build/listener
 sleep 5
+gnome-terminal -- roslaunch apriltag_ros continuous_detection.launch camera_name:=/camera/color/ image_topic:=image_raw
+sleep 3
 echo "all dependent programs launched. Press play in Gazebo and the launch your control station!"
