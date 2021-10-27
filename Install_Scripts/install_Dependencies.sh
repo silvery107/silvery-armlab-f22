a6b2631cfaf09bbf7809b0a6b50d6ef8e72e8153
Xiaotong Chen <cxt@umich.edu>
2021-10-27 15:36:32 -0400

    update librealsense apt-key

--- Install_Scripts/install_Dependencies.sh
+++ Install_Scripts/install_Dependencies.sh
@@ -42,7 +42,7 @@ rosdep update
 
 #install realsense software
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
-sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
+sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
 sudo apt-get install -y librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg ros-melodic-realsense2-camera
 
 #install apriltags
