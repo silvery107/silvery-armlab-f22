1d5ee5be8e7995cd242f6f1dcf3ccf105d2294c4
SilverSoul <1079931505@qq.com>
2022-09-09 00:20:18 -0400

    fix absolute path bug

--- gazebo_shim_setup.sh
+++ gazebo_shim_setup.sh
@@ -15,9 +15,9 @@ echo "%%%%%%%%%%%%% DONE installing gazebo-realsense plugin %%%%%%%%%%%%%"
 cd ../../..
 echo "%%%%%%%%%%%%% running xml macros & generating URDF's %%%%%%%%%%%%%"
 cd URDFs
-test -f "$1/src/interbotix_ros_arms/interbotix_descriptions/urdf/rx200.urdf.xacro"
-cp rx200.urdf.xacro $1/src/interbotix_ros_arms/interbotix_descriptions/urdf/rx200.urdf.xacro
-rosrun xacro xacro $1/src/interbotix_ros_arms/interbotix_descriptions/urdf/rx200.urdf.xacro > robot.urdf
+# test -f "$1~/interbotix_ws/src/interbotix_ros_arms/interbotix_descriptions/urdf/rx200.urdf.xacro"
+cp rx200.urdf.xacro ~/interbotix_ws/src/interbotix_ros_arms/interbotix_descriptions/urdf/rx200.urdf.xacro
+rosrun xacro xacro ~/interbotix_ws/src/interbotix_ros_arms/interbotix_descriptions/urdf/rx200.urdf.xacro > robot.urdf
 rosrun xacro xacro ./550board.sdf.xacro uri:=$PWD/RXArm_Board.obj > 550board.sdf
 rosrun xacro xacro ./robot_tag.sdf.xacro uri:=$PWD/TagStd41h12_id01.obj > robot_tag.sdf
 echo "%%%%%%%%%%%%% DONE running xml macros & generating URDF's %%%%%%%%%%%%%"
