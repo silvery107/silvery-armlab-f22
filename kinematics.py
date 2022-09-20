41381e8382e01c1be221170b075c020a6c2b312b
SilverSoul <44640904+silvery107@users.noreply.github.com>
2022-09-20 13:15:47 -0400

    move quat to utils.py

--- kinematics.py
+++ kinematics.py
@@ -10,7 +10,7 @@ import numpy as np
 # expm is a matrix exponential function
 from scipy.linalg import expm
 
-from utils import DTYPE
+from utils import DTYPE, Quaternion
 
 
 def clamp(angle):
@@ -227,31 +227,3 @@ def quat_to_rpy(q) -> np.ndarray:
 def rot_to_rpy(R):
     return quat_to_rpy(rot_to_quat(R))
 
-class Quaternion:
-    def __init__(self, w=1, x=0, y=0, z=0):
-        self.w = float(w)
-        self.x = float(x)
-        self.y = float(y)
-        self.z = float(z)
-        self._norm = np.sqrt(self.w*self.w+self.x*self.x+self.y*self.y+self.z*self.z, dtype=DTYPE)
-
-    def toNumpy(self):
-        """convert to an (4,1) numpy array"""
-        return np.array([self.w,self.x,self.y,self.z], dtype=DTYPE).reshape((4,1))
-    
-    def unit(self):
-        """return the unit quaternion"""
-        return Quaternion(self.w/self._norm,self.x/self._norm,self.y/self._norm,self.z/self._norm)
-
-    def conjugate(self):
-        return Quaternion(self.w, -self.x, -self.y, -self.z)
-    
-    def reverse(self):
-        """return the reverse rotation representation as the same as the transpose op of rotation matrix"""
-        return Quaternion(-self.w,self.x,self.y,self.z)
-
-    def inverse(self):
-        return Quaternion(self.w/(self._norm*self._norm),-self.x/(self._norm*self._norm),-self.y/(self._norm*self._norm),-self.z/(self._norm*self._norm))
-    
-    def __str__(self) -> str:
-        return '['+str(self.w)+', '+str(self.x)+', '+str(self.y)+', '+str(self.z)+']'
