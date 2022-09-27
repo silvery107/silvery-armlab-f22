fix block ori sign

camera.py
@@ -139,9 +139,9 @@ class Camera():
             cv2.putText(self.ProcessVideoFrame, self.color_id[color], (cx-30, cy+30), self.font, 0.5, (0,0,0), thickness=2)
             cv2.putText(self.ProcessVideoFrame, self.size_id[size], (cx-30, cy+45), self.font, 0.5, (0,0,0), thickness=2)
             # cv2.putText(self.ProcessVideoFrame, "+", (cx-12, cy+8), self.font, 1, (0,0,0), thickness=2)
-            cv2.putText(self.ProcessVideoFrame, str(idx), (cx-30, cy+75), self.font, 1, (0,0,0), thickness=2)
-            cv2.putText(self.VideoFrame, str(int(np.rad2deg(theta))), (cx, cy), self.font, 0.5, (0,0,0), thickness=2)
-            cv2.putText(self.ProcessVideoFrame, "%.0f"%(point[2]), (cx-20, cy+55), self.font, 0.5, (0,0,0), thickness=2)
+            # cv2.putText(self.ProcessVideoFrame, str(idx), (cx-30, cy+75), self.font, 1, (0,0,0), thickness=2)
+            cv2.putText(self.ProcessVideoFrame, str(int(np.rad2deg(theta))), (cx, cy), self.font, 0.5, (255,255,255), thickness=1)
+            # cv2.putText(self.ProcessVideoFrame, "%.0f"%(point[2]), (cx-20, cy+55), self.font, 0.5, (0,0,0), thickness=2)
 
         cv2.drawContours(self.ProcessVideoFrame, self.block_detections.all_contours, -1, (255, 0, 0), 1)
         cv2.drawContours(self.ProcessVideoFrame, self.block_detections.contours, -1, (0, 0, 255), 1)
@@ -313,7 +313,7 @@ class Camera():
                 self.block_detections.sizes.append(0) # 0 for small
             else:
                 self.block_detections.sizes.append(1) # 1 for large
-            block_ori = cv2.minAreaRect(contours_new_valid)[2] + 90 # turn the range from [-90, 0) to (0, 90]
+            block_ori = - cv2.minAreaRect(contours_new_valid)[2] # turn the range from [-90, 0) to (0, 90]
             self.block_detections.uvds.append([cx, cy, cz])
             self.block_detections.xyzs.append(self.coor_pixel_to_world(cx, cy, cz))
             self.block_detections.contours.append(contours_new_valid)