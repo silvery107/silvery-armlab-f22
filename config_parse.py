a13060819f09c9b0d63ce5f5387486b65cf7aa37
SilverSoul <z1079931505@gmail.com>
2022-09-28 22:45:14 -0400

    removed unused config parse func

--- config_parse.py
+++ config_parse.py
@@ -32,16 +32,3 @@ def parse_pox_param_file(pox_config_file):
     S_list = np.asarray([line.split() for line in f_line_contents[6:]], dtype=DTYPE)
     S_list = S_list.astype(float)
     return M_matrix, S_list
-
-def parse_pox_param_file_yaml(pox_config_file):
-    assert pox_config_file is not None
-    data = None
-    with open(pox_config_file, "r") as stream:
-        data = yaml.safe_load(stream)
-    assert (data is not None)
-    M_matrix_shape = (int(data["M_matrix"]["rows"]), int(data["M_matrix"]["cols"]))
-    M_matrix = np.asarray(data["M_matrix"]["data"], dtype=DTYPE).reshape(M_matrix_shape)
-    screw_vectors_shape = (int(data["screw_vectors"]["rows"]), int(data["screw_vectors"]["cols"]))
-    screw_vectors = np.asarray(data["screw_vectors"]["data"], dtype=DTYPE).reshape(screw_vectors_shape)
-    return M_matrix, screw_vectors
-
